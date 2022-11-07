b2378825ffbf6d6c47469005fe5bdf7b24e92736
Shakil Khan <52084536+shakilkhan496@users.noreply.github.com>
2022-11-08 03:07:59 +0600

    mongodb added

--- index.js
+++ index.js
@@ -8,6 +8,39 @@ app.get('/', (req, res) => {
     res.send(`Server is running on port ${port}`);
 })
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://<username>:<password>@cluster0.83izqje.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
+async function run() {
+    try {
+        console.log('database connection');
+
+
+    }
+    finally {
+        // perform
+    }
+
+}
+run().catch(console.dir)
+
+
+
+
+
+
+
+
+
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
