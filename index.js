0e4007714ed908c496c0940a98fd4ef30fb17220
Shakil Khan <52084536+shakilkhan496@users.noreply.github.com>
2022-11-08 14:32:02 +0600

    reviews added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.83izqje.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -21,6 +21,7 @@ async function run() {
     try {
         console.log('database connection');
         const servicesCollection = client.db("jacksPhotography").collection("services");
+        const reviewsCollection = client.db("jacksPhotography").collection("reviews");
 
         //get limit services
         app.get('/servicesLimit', async (req, res) => {
@@ -28,9 +29,10 @@ async function run() {
             const cursor = servicesCollection.find(query).limit(3);
             const services = await cursor.toArray();
             res.send(services);
-
-
         })
+
+
+        //all services
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = servicesCollection.find(query);
@@ -38,6 +40,36 @@ async function run() {
             res.send(services);
 
 
+        })
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+
+        })
+
+
+        //post reviews
+        app.post('/customerReviews', async (req, res) => {
+            const review = req.body;
+            console.log(review);
+            const result = await reviewsCollection.insertOne(review);
+            res.send(result);
+        })
+        //get reviews by productID
+        app.get('/customerReviews', async (req, res) => {
+            let query = {};
+            if (req.query.productID) {
+                query = {
+                    productID: req.query.productID
+                }
+            }
+            const cursor = reviewsCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+            console.log(query);
         })
 
 
